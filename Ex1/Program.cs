0b5a9b592f5d4f44054670f85804deee18cf5e77
hungkito <hungpvth2008007@fpt.edu.vn>
2021-06-18 14:46:11 +0700

    second commit

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Ex1
+﻿using System;
+
+namespace Ex1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Person person = new Person();
+            person.Name = "Anar";
+            person.Age = 40;
+            Console.WriteLine("Person deatails - Name = " +person.Name+", Age = "+person.Age);
+            Console.WriteLine("Person deatails (After incrementing age) - Name = " +person.Name+", Age = "+(person.Age+1));
         }
     }
 }
